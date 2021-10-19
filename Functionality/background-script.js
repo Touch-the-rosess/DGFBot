4416124e9b000211ad2b2790ca18f8d11f2ff9e7
Some person <WhyINeed.this@yandex.com>
2021-10-19 19:45:56 +0300

    Workflow commits
    Third commit is next:
    
    Is about fully recreating the "popupFunctionality.js" and "popupApearance.js", better say "popupApearance.js" is completly deleted and all it's code were refactored and pasted in "popupFunctionality.js".  In "popupApearance.js"  i created a async function named "Popup" that run code in secuential way with help of await. The async function "Popup" is doing:
    at first checking if "Dream girlfriend" tab exists, if it exists then we show the layout else just show the alert;
    the second it is setting the "switches on popup page" acordingly to it's statement stored in "background script" local.storage;
    the third is that it set "onclick event" on 'switches" that would handel what would happend if the switches would get triggered. ( right now i have just one "switch" named "beoutify", but when there would apear much more then i'll need to refactor the code on "popupFunctionality.js and background-script.js" acordingly to what i'll need to add )
    
    And the most importand is that i establyshed comunication betwen "backkground-script.js" and "popupFunctionality.js", with help of "browser.runtime.onMessage.addListener(handleMessage);" and "browser.runtime.sendMessage({withMyObjectAsMessage})".
    
    And i deleted the file "popupApearance.js" becouse i don't need in it.

--- Functionality/background-script.js
+++ Functionality/background-script.js
@@ -1,54 +1,58 @@
 console.log("Background is running.")
-currentTab = null;
-browser.tabs.onActivated.addListener((event)=> currentTab = event.tabId);
-setInterval(updateBroeseTime,1000)
 
-//Var init, those vars is just for proper showing of the pages
-//localStorage is saving things in string type, when getting var i need to convert it in Num type
-if (localStorage.getItem("HideHeaderBool") != null)
-{
-	localStorage.clear(); //I clear the local storage at the start of the background cuz the localStorage isn't temporary it is persistent and for some of my vars i need em to be temporary
-	console.log("Some of my hypothesys is true, but idk how to verify at the moment of creation, i need to prove it at the moment when it'll be compiled. The hiphothesys is that the local is persistent and i need to clear it everytime i start the backgroud script, cuz i need all my vars to be temp.")
+//this listener would be global listener for mesaging betwen popup-background, content-background
+function onError(error) {console.log(`Error handler of background catched an error:\n\t ${error}`);}
 
-}
-localStorage.setItem("HideHeaderBool", "True");
-localStorage.setItem("HideFooterBool", "True");
-localStorage.setItem("HideInformationBool", "False");
-localStorage.setItem("HideCampaignsBool", "False");
-localStorage.setItem("BeautifySwitch", "False");
-localStorage.setItem("Debug", "True");
+browser.runtime.onMessage.addListener(handleMessage);
 
-//if (localStorage.getItem("Debug") == "True"){console.log()}
-//if (localStorage.getItem("Debug") == "True"){
-
-async function updateBroeseTime(){
-	if(!currentTab){
-		return;
-	}
-	let frames = null;
-	try{
-		frames = await browser.webNavigation.getAllFrames({"tabId": currentTab})
-	} catch (error){
-		if (localStorage.getItem("Debug") == "True"){console.log(error);}
-	}
-	
-	let frame = frames.filter ((frame) => frame.parentFrameId == -1)[0];
-	
-	if (!frame.url.startsWith('http'))
-		return;
-	let hostname = new URL(frame.url).hostname;
-	
-	try{
-		let IsNizi = await browser.storage.local.get({["isNizi"]:"0"});
-		if (hostname.localeCompare("us-nizi2d-app.amz-aws.jp") === 0){
-			localStorage.setItem('isNizi', 'True');//If isNizi = 1 then user is in site dream girlfriend
-			return
-		}else{
-			localStorage.setItem('isNizi', 'False');//else isNizi = 0 then the user is not in site dream girlfriend (and we make a little log)
-			if (localStorage.getItem("Debug") == "True"){console.log("You've switched to '" + hostname + "' site.")}
+function handleMessage(request, sender, sendResponse) {
+	var WhoSummoned = request.sender;
+	console.log("Message from \" " + WhoSummoned + "\".\nWith whatToDo = " + request.whatToDo);
+	if (WhoSummoned == "PopupPage")
+	{
+		console.log("Popup sended a message");
+		var m = sendResponse; // reasigning function (without it calling) for further use
+		if (request.whatToDo == "CheckIfDGTabIsAvailable")
+		{
+			let tabs = browser.tabs.query({currentWindow: true}); // get all tabs from curent window
+			tabs.then(TabsParsing, onError);
+			function TabsParsing(tabs) { 
+				for (let tab of tabs) //parse them
+				{ //and when we find the needed url we sendResponse
+					if (tab.url == "https://us-nizi2d-app.amz-aws.jp/pc_room/"){m({response: request.whatToDo,niziTabId: tab.id});}// at the moment when we find the id we just send response
+				}  
+				m({response: request.whatToDo,niziTabId: null}); // if the url wasnt find then we just sent "niziTabId" as null
+			}
 		}
-	}catch (error){
-		if (localStorage.getItem("Debug") == "True"){console.log(error)}
+		if (request.whatToDo == "GetBeoutifyCheckboxVar")
+		{
+			try 
+			{
+				var vars = JSON.parse(localStorage.getItem('PopupVars'));
+				var myVar = vars.BeoutifyVar;
+				m({response: request.whatToDo,variable: myVar});
+			} 
+			catch (error) 
+			{
+				// if item vars don't exist in localStorage of background script then i'll init it with default needed data
+				var vars = {BeoutifyVar:1,HeaderVar:1,FooterVar:1,InformationVar:0,CampaignsVar:0}
+				localStorage.setItem("PopupVars", JSON.stringify(vars)); 
+				m({response: request.whatToDo,variable: vars.BeoutifyVar});
+			}
+		}
+		if (request.whatToDo == "SetBeoutifyCheckboxVar")
+		{
+			var vars = JSON.parse(localStorage.getItem("PopupVars"));
+			vars.BeoutifyVar = request.changeTo;
+			localStorage.setItem("PopupVars",JSON.stringify(vars));
+		}
+		if (request.whatToDo == "GetOthersCheckboxVar")
+		{
+			var vars = JSON.parse(localStorage.getItem("PopupVars"));
+			delete vars.BeoutifyVar;
+			m({response: request.whatToDo,variables: JSON.stringify(vars)});
+		}
+		return true; // keep the messaging channel open for sendResponse
+		// i need this return to recreate it as an "return promis" and after this i need to paste all my code above ( the code from "checkifavailable")		
 	}
 }
-//console.log("This is the fin of the background script, and is just running listener with some event.")
--- Functionality/popup/PopupPage/popupFunctionality.js
+++ Functionality/popup/PopupPage/popupFunctionality.js
@@ -1,29 +1,5 @@
 //this file would be just for storing some funtions, events with those funciton would be in the html files
-
-function BeautifyFunction()
-{
-	
-	if(document.getElementById('Checkbox1').checked) {
-		//browser.tabs.executeScript({code : 'document.getElementById("game_header").style.display = "none";document.getElementById("footer").style.display = "none";browser.storage.local.set({["isBeautifySwitched"]:1})'});
-		if (localStorage.getItem("Debug") == "True"){console.log("Now the beautify checkbox is checked")}
-		localStorage.setItem("BeautifySwitch", "True");
-		if (localStorage.getItem("HideHeaderBool") == "True"){browser.tabs.executeScript({code : 'document.getElementById("game_header").style.display = "none";'});}
-		if (localStorage.getItem("HideFooterBool") == "True"){browser.tabs.executeScript({code : 'document.getElementById("footer").style.display = "none";'});}
-		if (localStorage.getItem("HideInformationBool") == "True"){browser.tabs.executeScript({code : 'var contents = document.getElementById("right").contentWindow.document.getElementById("contents");var info = contents.children[0].children[0].children[0].children[2].style.display = "none";var infobanner = contents.children[0].children[0].children[0].children[3].style.display = "none";'});}
-		if (localStorage.getItem("HideCampaignsBool") == "True"){browser.tabs.executeScript({code : 'var contents = document.getElementById("right").contentWindow.document.getElementById("contents");var camp = contents.children[0].children[0].children[0].children[4].style.display = "none";var campbanner = contents.children[0].children[0].children[0].children[5].style.display = "none";'});}
-		
-	} else {	
-		//browser.tabs.executeScript({code: 'document.getElementById("game_header").style.display = "block";document.getElementById("footer").style.display = "block";browser.storage.local.set({["isBeautifySwitched"]:0})'});
-		if (localStorage.getItem("Debug") == "True"){console.log("Now the beautify checkbox isn't checked")}
-		localStorage.setItem("BeautifySwitch", "False");
-		if (localStorage.getItem("HideHeaderBool") == "True"){browser.tabs.executeScript({code : 'document.getElementById("game_header").style.display = "block";'});}
-		if (localStorage.getItem("HideFooterBool") == "True"){browser.tabs.executeScript({code : 'document.getElementById("footer").style.display = "block";'});}
-		if (localStorage.getItem("HideInformationBool") == "True"){browser.tabs.executeScript({code : 'var contents = document.getElementById("right").contentWindow.document.getElementById("contents");var info = contents.children[0].children[0].children[0].children[2].style.display = "block";var infobanner = contents.children[0].children[0].children[0].children[3].style.display = "block";'});}
-		if (localStorage.getItem("HideCampaignsBool") == "True"){browser.tabs.executeScript({code : 'var contents = document.getElementById("right").contentWindow.document.getElementById("contents");var camp = contents.children[0].children[0].children[0].children[4].style.display = "block";var campbanner = contents.children[0].children[0].children[0].children[5].style.display = "block";'});}
-
-	}
-}
-
+/*
 function LikeChatScript()
 {
 	browser.tabs.executeScript({file: "../../scripts/Init.js",allFrames: false});
@@ -36,5 +12,58 @@ function LikeChatScript()
 
 document.getElementById('Checkbox1').addEventListener('change', BeautifyFunction);
 document.getElementById('Button1').addEventListener('click', LikeChatScript)
+*/
+var NiziTabId = null;  // i need this var to be global cuz for further use, in future i'll need it to send "content script" to certain tab
 
+Popup();
+async function Popup()
+{
+	function handleResponse(message) {// i'll make handleResponse also to be global
+		if(message.response == "CheckIfDGTabIsAvailable"){NiziTabId = message.niziTabId;console.log("Nizi site is on tab with id: " + message.niziTabId);return;}
+		if(message.response == "GetBeoutifyCheckboxVar"){if (message.variable == 1){document.getElementById("Checkbox1").checked = true;}console.log("Popup var named beoutify had in it that data: " + message.variable);return;}// we don't need else (for if message.variable) cuz every time we reopen popup, we need to reinitialyze page
+		if(message.response == "GetOthersCheckboxVar"){return JSON.parse(message.variables);}
+	}
+
+	function handleError(error) {console.log(`Error handler of popup catched an error:\n\t ${error}`);}
+
+
+	var isTabAvailable =  browser.runtime.sendMessage({sender:"PopupPage", whatToDo:"CheckIfDGTabIsAvailable"});// i would send this object to the background script 
+	await isTabAvailable.then(handleResponse, handleError); // becouse "handleResponse" have return this await would make programm to runc line by line even if this is async
+	
+	if(NiziTabId != null)
+	{
+		var clases = document.getElementsByClassName("allBox");for(var i = 0; i < clases.length;i++){clases[i].style.display = "block";} // show boxes
+		// and now need to set switches acordingly to vars that would be stored in local.storage
+		var getStateOfVars = browser.runtime.sendMessage({sender:"PopupPage",whatToDo:"GetBeoutifyCheckboxVar"});
+		await getStateOfVars.then(handleResponse, handleError);
+		document.getElementById('Checkbox1').addEventListener('change', BeautifySwitchOnChangeFunction);
+		
+	} else  {document.getElementById("Alert").style.display = "block";} //show alert
+	
+	async function BeautifySwitchOnChangeFunction()
+	{
+		
+		if(document.getElementById('Checkbox1').checked) {
+			//browser.tabs.executeScript({code : 'document.getElementById("game_header").style.display = "none";document.getElementById("footer").style.display = "none";browser.storage.local.set({["isBeautifySwitched"]:1})'});
+			/*var setBeoutifyVar =  */browser.runtime.sendMessage({sender:"PopupPage", whatToDo:"SetBeoutifyCheckboxVar",changeTo:1});
+			var getOtherVars = browser.runtime.sendMessage({sender:"PopupPage", whatToDo:"GetOthersCheckboxVar"}); // by others i mean Header, footer, information and campaign
+			var vars = await getOtherVars.then(handleResponse, handleError);
+			if (vars.HeaderVar){browser.tabs.executeScript(NiziTabId,{code:'document.getElementById("game_header").style.display = "none";'});}
+			if (vars.FooterVar){browser.tabs.executeScript(NiziTabId,{code:'document.getElementById("footer").style.display = "none";'});}
+			if (vars.InformationVar){browser.tabs.executeScript(NiziTabId,{code:'var contents = document.getElementById("right").contentWindow.document.getElementById("contents");var info = contents.children[0].children[0].children[0].children[2].style.display = "none";var infobanner = contents.children[0].children[0].children[0].children[3].style.display = "none";'});}
+			if (vars.CampaignsVar){browser.tabs.executeScript(NiziTabId,{code:'var contents = document.getElementById("right").contentWindow.document.getElementById("contents");var camp = contents.children[0].children[0].children[0].children[4].style.display = "none";var campbanner = contents.children[0].children[0].children[0].children[5].style.display = "none";'});}		
+		} else {	
+			//browser.tabs.executeScript({code: 'document.getElementById("game_header").style.display = "block";document.getElementById("footer").style.display = "block";browser.storage.local.set({["isBeautifySwitched"]:0})'});
+			/*var setBeoutifyVar =  */browser.runtime.sendMessage({sender:"PopupPage", whatToDo:"SetBeoutifyCheckboxVar",changeTo:0});
+			var getOtherVars = browser.runtime.sendMessage({sender:"PopupPage", whatToDo:"GetOthersCheckboxVar"}); // by others i mean Header, footer, information and campaign
+			var vars = await getOtherVars.then(handleResponse, handleError);
+			if (vars.HeaderVar){browser.tabs.executeScript(NiziTabId,{code:'document.getElementById("game_header").style.display = "block";'});}
+			if (vars.FooterVar){browser.tabs.executeScript(NiziTabId,{code:'document.getElementById("footer").style.display = "block";'});}
+			if (vars.InformationVar){browser.tabs.executeScript(NiziTabId,{code:'var contents = document.getElementById("right").contentWindow.document.getElementById("contents");var info = contents.children[0].children[0].children[0].children[2].style.display = "block";var infobanner = contents.children[0].children[0].children[0].children[3].style.display = "block";'});}
+			if (vars.CampaignsVar){browser.tabs.executeScript(NiziTabId,{code:'var contents = document.getElementById("right").contentWindow.document.getElementById("contents");var camp = contents.children[0].children[0].children[0].children[4].style.display = "block";var campbanner = contents.children[0].children[0].children[0].children[5].style.display = "block";'});}		
+		}
+	}
+	
+	
+}
 
